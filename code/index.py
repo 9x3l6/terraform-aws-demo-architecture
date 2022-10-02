9754a17fa891345359dba5cb96aca8a40f6fb642
Youtube Sucks <youtubesucks@censorthis.youfuckers>
2022-10-02 18:51:13 -0500

    update lambda handler script

--- code/index.py
+++ code/index.py
@@ -4,10 +4,7 @@ import boto3
 
 def lambda_handler(event, context):
   region = os.environ['AWS_REGION']
-  if type(event['instances']) == str:
-    instances = [instance.strip() for instance in event['instances'].split(',') if instance]
-  else:
-    instances = [instance.strip() for instance in event['instances'] if instance]
+  instances = [instance.strip() for instance in event['instances'].split(',') if instance]
   ec2 = boto3.client('ec2', region_name=region)
   if event['action'] == 'stop':
     ec2.stop_instances(InstanceIds=instances)
